ae6e7f16db356cf1ba0fbb2f6d2123b5d99eb2b7
Christopher Read <christopherread22@outlook.com>
2020-12-07 23:44:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,10 @@ def using_concat(array, element)
   array.concat(element)
 end
 
+def using_insert(dogs, another_dog, insert_at)
 dogs = [Dog.new(1), Dog.new(4), Dog.new(10)]
 
 another_dog = Dog.new(8)
 insert_at = dogs.bsearch_index { |dog| dog.age >= another_dog.age }
 dogs.insert(insert_at, another_dog)
-puts dogs.inspect
+end
