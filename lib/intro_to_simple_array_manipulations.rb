d4a90e50f97ecac0959c439c07030784b20873a0
Christopher Read <christopherread22@outlook.com>
2020-12-07 23:18:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,6 @@ def using_concat(array, element)
   array.concat(element)
 end
 
-def using_insert(array, element)
-  using_insert = index
- array.insert(index)
+def using_uniq(array)
+  array.uniq(array)
 end
