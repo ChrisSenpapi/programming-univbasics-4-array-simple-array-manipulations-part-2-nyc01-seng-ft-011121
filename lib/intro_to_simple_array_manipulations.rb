0bd7b6676c6e4c5ea08916de7b13605bb7953ab6
Christopher Read <christopherread22@outlook.com>
2020-12-07 23:35:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,7 @@ def using_concat(array, element)
   array.concat(element)
 end
 
-def using_insert(array, new_element)
-  array.insert(new_element)
+def using_insert(para1, para2, array, new_element)
+  
+ 
 end
